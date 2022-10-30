c0b674aeac392246bd8e4f93ce6d049618cedf35
ThePianist <cafarimo@outlook.com>
2022-10-30 20:50:16 -0300

    Clear the screen

--- kilo.c
+++ kilo.c
@@ -50,6 +50,12 @@ char editorReadKey() {
   return c;
 }
 
+/*** output ***/
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
 /*** input ***/
 
 void editorProcessKeypress() {
@@ -68,8 +74,8 @@ int main() {
   enableRawMode();
 
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
-
   return 0;
 }
