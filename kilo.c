6e8f915971f63ad76205786125c3063ae495fb9c
ThePianist <cafarimo@outlook.com>
2022-11-04 19:33:19 -0300

    Insert ordinary characters

--- kilo.c
+++ kilo.c
@@ -218,6 +218,24 @@ void editorAppendRow(char *s, size_t len) {
   E.numRows++;
 }
 
+void editorRowInsertChar(editorRow *row, int at, int c) {
+  if (at < 0 || at > row->size) at = row->size;
+  row->chars = realloc(row->chars, row->size + 2);
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = c;
+  editorUpdateRow(row);
+}
+
+/*** editor operations ***/
+
+void editorInsertChar(int c) {
+  if (E.cursorY == E.numRows)
+    editorAppendRow("", 0);
+  editorRowInsertChar(&E.row[E.cursorY], E.cursorX, c);
+  E.cursorX++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename) {
@@ -445,6 +463,9 @@ void editorProcessKeypress() {
     case ARROW_LEFT:
     case ARROW_RIGHT:
       editorMoveCursor(c);
+      break;
+    default:
+      editorInsertChar(c);
       break;
   }
 }
